548b8ed192d4f412739b5ba92eaec9d645f906a0
Lyubomir Anastasov <lubomir.anastasov@gmail.com>
2015-01-27 19:12:41 +0200

    works throgh localhost or configure @match otherwise

---
+++ redmine-parent-child.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         parent, child commit through redmine 
+// @namespace    http://your.homepage/
+// @version      0.1
+// @description  enter something useful
+// @author       lanastasov
+// @match        http://localhost:8080/redmine/projects/*/repository/revisions/*/diff
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    
+    const version = "0.0.1";
+    
+	var prev = "";
+    var next = "";
+    var url_ = window.location.href.split("/").splice(0,9).join("/");
+    
+    $.ajax({
+        url: url_,
+        type: 'get',
+        success: function(data){
+            var el = $( '<div></div>' );
+            el.html(data);
+            prev = $(".contextual a", el)[0].href
+            next = $(".contextual a", el)[1].href	
+        }
+    });
+    
+    window.addEventListener("keydown", function(event) {
+        // pageup
+        if (event.keyCode == 33) {
+            event.preventDefault();
+            window.location.href = next + "/diff";
+        }
+    
+        // pagedown
+        if (event.keyCode == 34) {
+            event.preventDefault();
+            window.location.href = prev + "/diff";
+        }
+    })
+})();
